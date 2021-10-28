9260ea3f20e799fa7f6943a7741e9f81f1922486
WIG216 <numbucliff04@gmail.com>
2021-10-28 12:52:57 +0100

    First Commit

---
+++ Dynamic_text.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $heading = "Includes" ;
+    ?>
+    <h1>
+    <?php  echo $heading;    ?>
+    </h1>
+</body>
+</html>
---
+++ math.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 5 + 5;
+echo '<br>';
+echo 5 * 5;
+echo '<br>';
+echo 5 + 5 * 10;
+echo '<br>';
+echo 5 * 5 / 20;
+
+?>
